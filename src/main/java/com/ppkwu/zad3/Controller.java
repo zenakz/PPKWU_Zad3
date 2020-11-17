a7c3a68ca62076307a5650cdff13b5d7fe1c0380
Zientak.David <Zenakz@github.com>
2020-11-17 15:49:43 +0100

    downloaded ical4j library and tested its functions

--- src/main/java/com/ppkwu/zad3/Controller.java
+++ src/main/java/com/ppkwu/zad3/Controller.java
@@ -1,8 +1,13 @@
 package com.ppkwu.zad3;
 
 import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Version;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -16,25 +21,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
+import java.util.Date;
 
 @RestController
 public class Controller {
 
 
     @GetMapping(value = "/calendar")
-    public ResponseEntity<Resource> downloadFile(int year, int month) throws FileNotFoundException {
-
+    public ResponseEntity<Resource> downloadFile(int year, int month) throws IOException {
+        if(year<0||month<0||month>12)throw new RuntimeException("wrong parameter values.");
+        String calendarUrl="http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok="+year+"&miesiac="+month+"&lang=1";
         FileInputStream fin = new FileInputStream("mycalendar.ics");
+        fin.close();
 
-        CalendarBuilder builder = new CalendarBuilder();
+        Calendar calendar = new Calendar();
+        calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
+        calendar.getProperties().add(Version.VERSION_2_0);
+        calendar.getProperties().add(CalScale.GREGORIAN);
 
-        try {
-            Calendar calendar = builder.build(fin);
-        } catch (IOException | ParserException e) {
-            e.printStackTrace();
-        }
+        FileOutputStream fout = new FileOutputStream("calendar.ics");
 
-        Resource resource = new ClassPathResource("mycalendar.ics");
+        CalendarOutputter outputter = new CalendarOutputter();
+        outputter.output(calendar, fout);
+        Resource resource = new ClassPathResource("/mycalendar.ics");
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")

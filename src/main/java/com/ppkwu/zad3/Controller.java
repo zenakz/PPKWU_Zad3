38a6d8b7190f2d3dcbd676d2f96305e738ade567
zenakz <57412399+zenakz@users.noreply.github.com>
2020-11-17 14:38:47 +0100

    Created project with an empty controller

---
+++ src/main/java/com/ppkwu/zad3/Controller.java
@@ -0,0 +1,10 @@
+package com.ppkwu.zad3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+}
---
+++ src/test/java/com/ppkwu/zad3/Zad3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppkwu.zad3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

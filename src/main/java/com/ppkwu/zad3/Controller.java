b3cb720609c5135593de1d61e0f97c19ce178cdf
Zientak.David <Zenakz@github.com>
2020-12-01 15:15:41 +0100

    fixed downloading calendar for some dates and refactored

--- src/main/java/com/ppkwu/zad3/Controller.java
+++ src/main/java/com/ppkwu/zad3/Controller.java
@@ -32,33 +32,19 @@ public class Controller {
 
 
     @GetMapping(value = "/calendar")
-    public void downloadFile(Integer year, Integer month, HttpServletResponse response) throws IOException, ParseException, URISyntaxException {
-        if(year<0||month<0||month>12)throw new RuntimeException("wrong parameter values.");
+    public void downloadFile(Integer year, Integer month, HttpServletResponse response) throws IOException {
+        if (year < 0 || month < 0 || month > 12) throw new RuntimeException("wrong parameter values.");
         String fixedMonth = month.toString();
-        if(fixedMonth.length()<2)fixedMonth = "0"+month;
-        String calendarUrl="http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok="+year+"&miesiac="+fixedMonth+"&lang=1";
-
-        Document doc = Jsoup.connect(calendarUrl).get();
-        Elements elements = doc.select("td");
-
-        Calendar calendar = new Calendar();
-        calendar.getProperties().add(new ProdId("-//Zientak//iCal4j 1.0//EN"));
-        calendar.getProperties().add(Version.VERSION_2_0);
-        calendar.getProperties().add(CalScale.GREGORIAN);
-        UidGenerator uidgen = () -> new Uid("testUid");
-        for (Element element : elements) {
-            if(element.attr("class").equals("active")) {
-                VEvent vEvent = new VEvent(new Date(year.toString() + fixedMonth + element.child(0).text()), element.getElementsByTag("p").text());
-                vEvent.getProperties().add(uidgen.generateUid());
-                Url url = new Url();
-                url.setValue(calendarUrl);
-                vEvent.getProperties().add(url);
-                calendar.getComponents().add(vEvent);
-            }
+        if (fixedMonth.length() < 2) fixedMonth = "0" + month;
+        String calendarUrl = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + fixedMonth + "&lang=1";
+        Calendar calendar = null;
+        try {
+            calendar = addEventsToCalendar(calendarUrl, year.toString(), fixedMonth);
+        } catch (ParseException | URISyntaxException e) {
+            e.printStackTrace();
         }
 
         FileOutputStream fout = new FileOutputStream("calendar.ics");
-
         CalendarOutputter outputter = new CalendarOutputter();
         outputter.output(calendar, fout);
 
@@ -66,5 +52,31 @@ public class Controller {
         IOUtils.copy(inputStream, response.getOutputStream());
         response.setContentType("text/calendar");
 
+    }
+
+    public Calendar addEventsToCalendar(String calendarUrl, String year, String month) throws ParseException, URISyntaxException, IOException {
+        Document doc = Jsoup.connect(calendarUrl).get();
+        Elements elements = doc.select("td");
+        Calendar calendar = new Calendar();
+        calendar.getProperties().add(new ProdId("-//Zientak//iCal4j 1.0//EN"));
+        calendar.getProperties().add(Version.VERSION_2_0);
+        calendar.getProperties().add(CalScale.GREGORIAN);
+        UidGenerator uidgen = () -> new Uid("testUid");
+        for (Element element : elements) {
+            if (element.attr("class").equals("active")) {
+                java.util.Calendar cal = java.util.Calendar.getInstance();
+                String day = element.getElementsByTag("a").html();
+                cal.set(java.util.Calendar.DAY_OF_MONTH, Integer.parseInt(day));
+                cal.set(java.util.Calendar.MONTH, Integer.parseInt(month));
+                cal.set(java.util.Calendar.YEAR, Integer.parseInt(year));
+                VEvent vEvent = new VEvent(new Date(cal), element.getElementsByTag("p").text());
+                vEvent.getProperties().add(uidgen.generateUid());
+                Url url = new Url();
+                url.setValue(calendarUrl);
+                vEvent.getProperties().add(url);
+                calendar.getComponents().add(vEvent);
+            }
+        }
+        return calendar;
     }
 }
